83b4e2d7c9b3a40bf5ba29fe5884ff01c11283f4
Chett Tiller <chett.tiller@gmail.com>
2019-12-10 11:16:38 -0500

    removed solution from create-kaiju-form

--- src/CreateKaijuForm.js
+++ src/CreateKaijuForm.js
@@ -1,37 +1,27 @@
-import React, {useState} from 'react'
+import React from 'react'
 
-const CreateKaijuForm = ({createKaiju}) => {
+class CreateKaijuForm extends React.Component {
 
-  const [name, setName] = useState('')
-  const [power, setPower] = useState('')
-  const [image, setImage] = useState('')
+  render() {
+    return (
+      <form id='create-kaiju-form'>
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    createKaiju({name, power, image})
-    setName('')
-    setPower('')
-    setImage('')
+        <label>Name: </label>
+        <input type='text' placeholder="add your name here.." />
+
+        <label>Power: </label>
+        <input type='text' placeholder="add your power here..." />
+
+        <label>Image: </label>
+        <input type='text' placeholder="add your image url here..." />
+
+        <br/>
+
+        <input type='submit' value='List Kaiju' />
+
+      </form>
+    )
   }
-
-  return (
-    <form id='create-kaiju-form' onSubmit={handleSubmit}>
-
-      <label>Name: </label>
-      <input type='text' value={name} placeholder="add your name here.." onChange={e => setName(e.target.value)} />
-
-      <label>Power: </label>
-      <input type='text' value={power} placeholder="add your power here..." onChange={e => setPower(e.target.value)} />
-
-      <label>Image: </label>
-      <input type='text' value={image} placeholder="add your image url here..." onChange={e => setImage(e.target.value)} />
-
-      <br/>
-
-      <input type='submit' value='Create Kaiju' />
-
-    </form>
-  )
 }
 
 export default CreateKaijuForm
